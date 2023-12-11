4085be94d8f5aeb5b0b5fe5dafb87a5e25876441
A3D <136644840+AdrieVanDijke@users.noreply.github.com>
2023-12-11 16:25:04 +0100

    Nieuw bestand toegevoegd: a3d/a3d_teksten.py

---
+++ a3d/a3d_teksten.py
@@ -0,0 +1,21 @@
+
+# tekst voor uitklapbaar gedeelte bovenaan de pagina
+extekst = """
+                Dit is een Vraag en Antwoord module die gebruik maakt van AI *(kunstmatige intelegentie)* om antwoorden op je vragen te geven.
+                Dit is geen Chat-bot en je kunt geen uitgebreide gesprekken voeren met deze AI simpelweg omdat als je een nieuwe vraag stelt de vorige vraag en het antwoord daarop niet in het geheugen worden opgeslagen.
+                - Type in het tekst veld hieronder je vraag in gewoon Nederalands en druk op **Enter** om de vraag te versturen.
+                - Probeer je vraag zo goed *(en duidelijk)* mogelijk te formuleren om de AI zo veel mogelijk bruikbare informatie te geven om een goed antwoord te kunnen geven. 
+                - Als je je vraag hebt verstuurd gaat er rechtsboven een animatie draaien om aan te geven dat de AI aan het werk is.
+                """
+
+# Antwoord: geen resultaat 
+antwoord = f"""
+            🤷‍♀️ Het spijt me maar ik kan het antwoord op je vraag "**{user_question}**" niet vinden. 
+            *Misschien kun je de vraag nog een keer in andere woorden stellen?* Of stel een nieuwe vraag en neem later contact met ons op:
+            - Je kunt *(indien gewenst)* op de "Service + Contact" pagina een belafspraak maken of *(onder werktijden)* live chatten met een van onze medewerkers.\n
+            💡Hier nog een paar links die je misschien verder kunnen helpen:
+            - [Onze kennisbank](https://kwaliteitsysteem.nl/kennisbank/) *Alles waar je als CAT-therapeut mee te maken hebt of kunt krijgen*
+            - [GRO: vind een geschikte opleiding](https://gatregisteropleidingen.nl) *Alternatieve zorg opleidingen, scholingen, module opleidingen en bij- en nascholingen*
+            - [Catcollectief.nl](https://catcollectief.nl/): [Profiel CAT-therapeut](https://catcollectief.nl/profiel/) 
+            - [Catvergoedbaar.nl](https://catvergoedbaar.nl/): [Profiel Vergoedbare CAT-therapeut](https://catvergoedbaar.nl/profiel/)
+            """
